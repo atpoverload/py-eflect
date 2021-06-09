90bb01e483c5b41b62f5289873a99e975f94d554
timurbey <atpoverload@gmail.com>
2021-06-09 16:40:54 -0600

    fixing bad import

--- eflect/eflect/processing/processing.py
+++ eflect/eflect/processing/processing.py
@@ -5,7 +5,7 @@ from sys import argv
 import numpy as np
 import pandas as pd
 
-from eflect.processing.preprocessing import read_app_data, read_cpu_data, read_energy_data, read_async_data
+from eflect.processing.preprocessing import process_app_data, process_cpu_data, process_energy_data, process_async_data
 
 def account_application_energy(app, cpu, energy):
     return energy * (app / cpu).replace(np.inf, 1).clip(0, 1)
@@ -18,15 +18,15 @@ def pre_process(data_dir):
     for f in os.listdir(os.path.join(data_dir)):
         df = pd.read_csv(os.path.join(data_dir, f), header = None)
         if 'ProcTaskSample' in f:
-            app.append(read_app_data(df))
+            app.append(process_app_data(df))
         elif 'ProcStatSample' in f:
-            cpu.append(read_cpu_data(df))
+            cpu.append(process_cpu_data(df))
         elif 'EnergySample' in f:
-            energy.append(read_energy_data(df))
+            energy.append(process_energy_data(df))
         elif 'AsyncProfilerSample' in f:
-            traces.append(read_async_data(df))
+            traces.append(process_async_data(df))
         elif 'StackTraceSample' in f:
-            traces.append(read_async_data(df))
+            traces.append(process_async_data(df))
 
     return pd.concat(app), pd.concat(cpu), pd.concat(energy), pd.concat(traces)
 
