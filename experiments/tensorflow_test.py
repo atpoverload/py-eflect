cc17b5d2bef14c23b245eb9bb78116472442cdc5
timurbey <atpoverload@gmail.com>
2021-06-09 19:51:13 -0600

    fixed tensorflow example

--- experiments/tensorflow_test.py
+++ experiments/tensorflow_test.py
@@ -12,23 +12,8 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(10)
 ])
 
-predictions = model(x_train[:1]).numpy()
-
-array([[-0.28274903,  0.19788507, -0.18761218,  0.80738944, -0.25398862,
-        -1.2106801 ,  0.08712098,  0.24884817,  0.48026958, -0.63544625]],
-        dtype=float32)
-
-array([[0.07085732, 0.11458333, 0.07792953, 0.21077827, 0.07292479,
-        0.02801492, 0.10256924, 0.12057421, 0.15197057, 0.04979781]],
-        dtype=float32)
-
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-loss_fn(y_train[:1], predictions).numpy()
-
 model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
-
 model.fit(x_train, y_train, epochs=5)
 model.evaluate(x_test,  y_test, verbose=2)
