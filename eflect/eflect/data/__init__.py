2601712e60a68cac4aa3a6dfed91b8f6e73442c5
timurbey <atpoverload@gmail.com>
2021-06-12 18:49:17 -0600

    removed yappi import

--- eflect/eflect/data/__init__.py
+++ eflect/eflect/data/__init__.py
@@ -1,9 +1,9 @@
 from eflect.data.sources import sample_tasks
 from eflect.data.sources import sample_cpu
 from eflect.data.sources import sample_rapl
-from eflect.data.sources import sample_yappi
+# from eflect.data.sources import sample_yappi
 
 from eflect.data.parser import parse_tasks_data
 from eflect.data.parser import parse_cpu_data
 from eflect.data.parser import parse_rapl_data
-from eflect.data.parser import parse_yappi_data
+# from eflect.data.parser import parse_yappi_data
