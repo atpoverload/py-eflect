165d8a84931a379a51c8d247b448bbf5854a417f
timurbey <atpoverload@gmail.com>
2021-06-27 16:18:35 -0600

    changed to correct field

--- eflect/eflect/data/yappi.py
+++ eflect/eflect/data/yappi.py
@@ -3,11 +3,12 @@ import pandas as pd
 import yappi
 
 def parse_yappi_data(data, threads):
-    """ Extracts all traces from the yappi and puts it into a DataFrame. """
+    """ Extracts all traces from yappi and puts it into a DataFrame. """
     df = []
     for thread in data:
         for trace in yappi.get_func_stats(ctx_id=thread.id):
-            df.append((threads[thread.tid], trace[8], trace[15]))
+            # grabs tsub from yappi YStats
+            df.append((threads[thread.tid], trace[7], trace[15]))
     df = pd.DataFrame(df)
     df.columns = ['id', 'calls', 'method']
 
