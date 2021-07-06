9a6bdf55f95131b9a2c1eaf9938c66c32b8062e5
timurbey <atpoverload@gmail.com>
2021-07-06 16:30:18 -0600

    fixed a typo

--- eflect/eflect/data/yappi.py
+++ eflect/eflect/data/yappi.py
@@ -4,7 +4,7 @@ import yappi
 
 def parse_yappi_data(data):
     """ Extracts all traces from yappi and puts it into a DataFrame. """
-    df = pd.DataFrame([[ts, id, trace[3], traces[7], trace[15]] for ts, id, trace in data])
+    df = pd.DataFrame([[ts, id, trace[3], trace[7], trace[15]] for ts, id, trace in data])
     df.columns = ['timestamp', 'id', 'tsub', 'calls', 'method']
 
     return df.set_index(['timestamp', 'id', 'method'])
