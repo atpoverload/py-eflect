a387d43833392dd34b13bfe169ae2e05d6f19d3c
timurbey <atpoverload@gmail.com>
2021-10-28 20:23:51 -0600

    more proto fixes

--- eflect/sample/sample_storage.py
+++ eflect/sample/sample_storage.py
@@ -1,13 +1,13 @@
 """ Object that collects eflect samples into an EflectDataSet. """
 
-from eflect.protos.sample.data_set_pb2 import EflectDataSet
+from eflect.protos.sample.sample_pb2 import DataSet
 from eflect.protos.sample.jiffies_pb2 import CpuSample, TaskSample
 from eflect.protos.sample.rapl_pb2 import RaplSample
 
 # TODO(timur): should all of this live in sampler.py?
 class SampleStorage:
     def __init__(self):
-        self.data_set = EflectDataSet()
+        self.data_set = DataSet()
         self.data = {
             CpuSample: self.data_set.cpu,
             TaskSample: self.data_set.task,
@@ -25,8 +25,8 @@ class SampleStorage:
         return self.data_set
 
 def load_data_set(data_set_path):
-    """ Loads an EflectDataSet from the path. """
+    """ Loads an DataSet from the path. """
     with open(data_set_path, 'rb') as f:
-        data_set = EflectDataSet()
+        data_set = DataSet()
         data_set.ParseFromString(f.read())
         return data_set
